4b2fcf2e7bc085c8e1de930deb43443542793958
jhon <jzapanaq@est.unap.edu.pe>
2024-06-17 12:43:45 -0500

    first commit

---
+++ app/src/main/java/zapana/quispe/resumendegastos/WelcomeActivity.kt
@@ -0,0 +1,30 @@
+package zapana.quispe.resumendegastos
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btncontinuar = findViewById<Button>(R.id.btncontinuar)
+        btncontinuar.setOnClickListener{
+            val Intent = Intent(this, LoginActivity:: class.java )
+            startActivity(Intent)
+
+        }
+    }
+}
